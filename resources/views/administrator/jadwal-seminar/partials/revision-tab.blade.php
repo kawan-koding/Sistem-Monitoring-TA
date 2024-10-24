1c3ef92a7a451ebe711026547f57a59cdeadd495
sadestyrahmadhani <sadestyrahmadhani012@gmail.com>
2024-10-24 16:24:41 +0700

    add view detail jadwal-seminar

---
+++ resources/views/administrator/jadwal-seminar/partials/revision-tab.blade.php
@@ -0,0 +1,47 @@
+<div class="row align-items-center">
+    <div class="col-md-9 col-12">
+        <h5 class="fw-bold mb-0">Lembar Revisi</h5>
+        <p class="text-muted small m-0">Lihat uraian revisi yang telah diberikan oleh dosen penguji.</p>
+    </div>
+    <div class="col-md-3 col-12 text-center">
+        <button class="btn btn-outline-dark btn-sm"><i class="bx bx-printer"></i> Cetak Lembar Revisi</button>
+    </div>
+</div>
+<hr>
+<table class="table" width="100%">
+    <thead class="bg-light">
+        <th width="25">No.</th>
+        <th>Penguji</th>
+        <th>Uraian</th>
+        <th></th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1.</td>
+            <td style="white-space: nowrap">
+                <strong>Dianni Yusuf, S.Kom., M.Kom.</strong>
+                <p class="m-0 text-muted small">Penguji 1</p>
+            </td>
+            <td>
+                <ol>
+                    <li>Perbaiki penulisan</li>
+                    <li>Perlu dibahas lebih detail terkait metode yang akan digunakan</li>
+                    <li>Pengujian belum mencakup aspek - aspek yang diperlukan dalam mengukur tingkat keberhasilan suatu sistem</li>
+                </ol>
+            </td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td style="white-space: nowrap">
+                <strong>Sepyan Purnama Kristanto, S.Kom., M.Kom.</strong>
+                <p class="m-0 text-muted small">Penguji 2</p>
+            </td>
+            <td>
+                <ol>
+                    <li>Perbaiki penulisan</li>
+                    <li>Gambar kurang jelas</li>
+                </ol>
+            </td>
+        </tr>
+    </tbody>
+</table>
